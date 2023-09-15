178e5263b2f12ea4007fc9ef1d21b307e6ec8b75
spector700 <72362399+spector700@users.noreply.github.com>
2023-09-14 20:31:53 -0500

    added to git

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template
+from requests_cache import CachedSession
+import requests
+
+app = Flask(__name__)
+
+# Default is to cache the response
+def get_response(url, cache=True):
+# Cache for the apod
+    if cache:
+        session = CachedSession(
+            cache_name='apod_cache',
+            # Cache expires after 1 hour
+            expire_after=60 * 60,
+        )
+    else:
+        session = requests.Session()
+
+
+    try:
+        response = session.get(url)
+        # Raise an exception if http error code
+        response.raise_for_status()
+        print(response)
+
+        # Turns the response into json
+        json_data = response.json()
+        return json_data
+
+    # If error print error
+    except Exception as error:
+        print(f'Error: {error}')
+        return None
+
+
+@app.route("/")
+def index():
+    iss_data = None
+
+    # Call request with cache (default)
+    apod_data = get_response('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+        
+    # Call iss without cache
+    iss_data = get_response('http://api.open-notify.org/iss-now.json', False)
+
+    return render_template("index.html", apod_data=apod_data, iss_data=iss_data)
+        
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
