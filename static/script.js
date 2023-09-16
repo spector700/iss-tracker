efc658123aeda7577a839c9d552ea8326b6a3fa4
spector700 <72362399+spector700@users.noreply.github.com>
2023-09-15 19:34:27 -0500

    - Added the iss to the map

--- static/script.js
+++ static/script.js
@@ -1,19 +1,29 @@
+// Changes the class of the sidbar buttons to active if clicked
 $(".sidebar ul li").on("click", function () {
   $(".sidebar ul li.active").removeClass("active");
   $(this).addClass("active");
 });
 
+
+// Define the icon params
+var ISSIcon = L.icon({
+  iconUrl: '/static/International_Space_Station.svg.png',
+  iconSize: [100, 100],
+  iconAnchor: [25, 15],
+  popupAnchor: [50, 25],
+});
+
 // Define the map outside the moveISS function and initialize it once
 var map = L.map("map").setView([0, 0], 4);
+var iss = L.marker([0, 0], {icon: ISSIcon}).addTo(map);
 
+// Initialize the tiles of the map
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-// Call the function
-moveISS();
 
 function moveISS() {
   $.getJSON(
@@ -25,12 +35,14 @@ function moveISS() {
       // Update the map
       map.setView([lat, lon]);
 
-      // The update to the map is done here:
-      // iss.setLatLng([lat, lon]);
-      // isscirc.setLatLng([lat, lon]);
-      // map.panTo([lat, lon], (animate = true));
+      // Update the icon 
+      iss.setLatLng([lat, lon]);
+      map.panTo([lat, lon], (animate = true));
     }
   );
   // Update every 3 sec
   setTimeout(moveISS, 3000);
 }
+
+// Call the function
+moveISS();
